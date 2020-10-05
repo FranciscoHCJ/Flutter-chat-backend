98a80f8e0660a218eb090e730194ad4937da43bd
Juan Francisco <juanfranciscoutn@gmail.com>
2020-10-04 20:12:41 -0500

    Backent chat terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,11 +1,28 @@
 const { io } = require('../index');
+const { comprobarJWT } = require('../helpers/jwt');
+const { usuarioConectado, usuarioDesconectado, grabarMensaje } = require('../controllers/socket');
 
 // Mensajes de Sockets
 io.on('connection', client => {
-    console.log('Cliente conectado');
+    const [valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
+
+    // Verificar autenticación
+    if ( !valido ) { return client.disconnect(); }
+
+    // Cliente autenticado
+    usuarioConectado(uid);
+
+    // Ingresar al usuario a una sala en particular
+    // sala global, client.id, 5f70e8b1e6e4c40ca57dfd71
+    client.join(uid);
+
+    client.on('mensaje-personal', async (payload) => {
+        await grabarMensaje( payload );
+        io.to(payload.para).emit('mensaje-personal', payload);
+    });
 
     client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+        usuarioDesconectado(uid);
     });
 
 });
